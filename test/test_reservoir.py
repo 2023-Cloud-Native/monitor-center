10140b01b24b16d9920809c1156898bc030819fc
Justin <justin900429@gmail.com>
2023-05-17 02:50:12 +0800

    Modify the test file

--- test/test_reservoir.py
+++ test/test_reservoir.py
@@ -14,5 +14,42 @@ def test_reservoir_fail():
 
 
 def test_reservoir_normal():
+    import datetime
+
     reservoir_manager = ReservoirManager()
     assert isinstance(reservoir_manager.data, dict)
+    for area in reservoir_manager.data:
+        assert isinstance(
+            reservoir_manager.data[area]["updated_time"], datetime.datetime
+        )
+        assert (
+            isinstance(reservoir_manager.data[area]["inflow"], float)
+            and reservoir_manager.data[area]["inflow"] >= 0
+        )
+        assert (
+            isinstance(reservoir_manager.data[area]["outflow"], float)
+            and reservoir_manager.data[area]["outflow"] >= 0
+        )
+        assert (
+            isinstance(reservoir_manager.data[area]["percentage"], float)
+            and reservoir_manager.data[area]["percentage"] >= 0
+            and reservoir_manager.data[area]["percentage"] <= 100
+        )
+        assert (
+            isinstance(reservoir_manager.data[area]["total_capacity"], float)
+            and reservoir_manager.data[area]["total_capacity"] >= 0
+        )
+        assert (
+            isinstance(reservoir_manager.data[area]["current_capacity"], float)
+            and reservoir_manager.data[area]["current_capacity"] >= 0
+        )
+        assert (
+            abs(
+                reservoir_manager.data[area]["percentage"]
+                - (
+                    reservoir_manager.data[area]["current_capacity"]
+                    / reservoir_manager.data[area]["total_capacity"]
+                )
+            )
+            < 1e-5
+        )
